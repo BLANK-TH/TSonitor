f1177284114b114fc3fd9395f0b482d72bf8141f
BLANK-TH <blankdev.th@gmail.com>
2021-12-31 06:17:59 -0500

    bump version number

--- helpers/gvars.py
+++ helpers/gvars.py
@@ -9,7 +9,7 @@ import re
 from .file import load_constants
 
 constants = load_constants()
-VERSION = '0.0.9'
+VERSION = '1.0.1'
 
 TTT_ROUND_REGEX = re.compile(constants["ttt"]["regex"]["round"])
 TTT_TIME_REGEX = re.compile(constants["ttt"]["regex"]["time"])
