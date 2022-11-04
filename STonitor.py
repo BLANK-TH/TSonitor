c1c5c8067068c0198b29db1d5a96777f5276956f
BLANK-TH <blankdev.th@gmail.com>
2022-11-03 21:14:31 -0400

    .

--- STonitor.py
+++ STonitor.py
@@ -4,7 +4,6 @@
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # ------------------------------------------------------------------------------
 
-import os
 import sys
 from datetime import datetime
 from time import sleep, time
